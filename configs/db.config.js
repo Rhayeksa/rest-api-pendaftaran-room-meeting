e7152022b96f7d3fec8366884214d36cfe7632a7
rhayeksa <rhayeksads@gmail.com>
2020-12-17 06:39:19 +0700

    change hosting

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  HOST: "127.0.0.1",
-  USER: "root",
-  PASSWORD: "",
-  DB: "db_room_meeting",
+  HOST: " sql12.freemysqlhosting.net",
+  USER: "sql12382542",
+  PASSWORD: "9GzelBGuRu",
+  DB: "sql12382542",
   dialect: "mysql",
 };
+// module.exports = {
+//   HOST: "127.0.0.1",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "sql12382542",
+//   dialect: "mysql",
+// };
